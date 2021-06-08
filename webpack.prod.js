a5e7a27e9a732e09a59f8de30dcad415a91a9e9f
ahmadSouqi <ahmadsos5009@hotmail.com>
2021-06-09 02:34:57 +0300

    performance improvement

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const {DefinePlugin} = require('webpack');
 const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -10,6 +11,10 @@ module.exports = merge(common, {
         new DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
+        new CopyPlugin({patterns:[
+            { from: 'public/google86229b14d00bfabe.html', to: 'google86229b14d00bfabe.html' },
+            { from: 'public/robots.txt', to: 'robots.txt' }
+        ]}),
     ],
     optimization: {
         minimize: true,
